b98f4b7cdaed1aaf9941ec26555cd275d9ec4fad
viniciusmarcon1 <vinimarcon5@gmail.com>
2025-06-03 16:47:30 -0300

    Add new exercises

---
+++ Exemplos/ex_17/ex_17.py
@@ -0,0 +1,37 @@
+# app.py
+from flask import Flask, render_template, redirect, url_for, request
+from login import login
+from sensors import sensors
+from actuators import actuators
+
+app= Flask(__name__)
+
+app.register_blueprint(login, url_prefix='/')
+app.register_blueprint(sensors, url_prefix='/')
+app.register_blueprint(actuators, url_prefix='/')
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+  
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/upload_file', methods = ['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('static/img/' + f.filename)
+        return "ok" 
+
+# @app.route('/actuators')
+# def actuators():
+#     return render_template("actuators.html", atuadores=atuadores)
+
+# @app.route('/sensors')
+# def sensors():
+#     return render_template("sensors.html", sensores=sensores)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080,debug=True)
